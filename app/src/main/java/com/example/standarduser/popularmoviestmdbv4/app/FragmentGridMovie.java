40be64cb4111c622285b7c53c4530183886f42e6
withered-flowers <wendy.hardianto@gmail.com>
2017-07-23 15:45:41 +0700

    Implement onSaveInstanceState

--- app/src/main/java/com/example/standarduser/popularmoviestmdbv4/app/FragmentGridMovie.java
+++ app/src/main/java/com/example/standarduser/popularmoviestmdbv4/app/FragmentGridMovie.java
@@ -36,10 +36,13 @@ import retrofit2.Response;
 public class FragmentGridMovie extends Fragment implements AdapterMovieObject.clickHandler {
   private static final String LOG_TAG = FragmentGridMovie.class.getSimpleName();
   private static final String PARCEL_TAG = "MovieObject";
+  private static final String SAVE_TAG = "MovieState";
 
   private Call<List_MovieObjects> callMovieList;
   private ProgressBar pbrMovieList;
   private RecyclerView rvwGridMovie;
+  
+  private List<MovieObject> listMovie = null;
 
   @Override
   public void onImageViewClick(MovieObject obj) {
@@ -80,40 +83,47 @@ public class FragmentGridMovie extends Fragment implements AdapterMovieObject.cl
     rvwGridMovie.setVisibility(View.INVISIBLE);
     pbrMovieList.setVisibility(View.VISIBLE);
 
-    if(!sortBy.equals(getString(R.string.action_gridmovie_sortby_option_favorite_value))) {
-      //Fetch the Data From TMDb
-      callMovieList = theEndpoint.getMoviesBySort(sortBy, BuildConfig.TMDB_API_KEY);
-
-      callMovieList.enqueue(new Callback<List_MovieObjects>() {
-        @Override
-        public void onResponse(Call<List_MovieObjects> call, Response<List_MovieObjects> response) {
-          List_MovieObjects listMovies = response.body();
-
-          if (listMovies != null) {
-            //Fill the data here
-            List<MovieObject> listMovie = listMovies.getListMovieObject();
-
-            allocateRecyclerView(listMovie, view);
-
-            //TODO Turn this on for debug purpose
+    if(savedInstanceState != null) {
+      listMovie = savedInstanceState.getParcelableArrayList(SAVE_TAG);
+      allocateRecyclerView(listMovie, view);
+  
+      rvwGridMovie.setVisibility(View.VISIBLE);
+      pbrMovieList.setVisibility(View.INVISIBLE);
+    }
+    else {
+      if (!sortBy.equals(getString(R.string.action_gridmovie_sortby_option_favorite_value))) {
+        //Fetch the Data From TMDb
+        callMovieList = theEndpoint.getMoviesBySort(sortBy, BuildConfig.TMDB_API_KEY);
+    
+        callMovieList.enqueue(new Callback<List_MovieObjects>() {
+          @Override
+          public void onResponse(Call<List_MovieObjects> call, Response<List_MovieObjects> response) {
+            List_MovieObjects listMovies = response.body();
+        
+            if (listMovies != null) {
+              //Fill the data here
+              listMovie = listMovies.getListMovieObject();
+          
+              allocateRecyclerView(listMovie, view);
+          
+              //TODO Turn this on for debug purpose
 //          Log.d(LOG_TAG, "Total Film is: " + listMovie.size());
 //
 //          for(int i=0; i<listMovie.size(); i++) {
 //            Log.d(LOG_TAG, "Film " + i + " is:" + listMovie.get(i).getObjectTitle());
 //          }
+            }
+        
+            rvwGridMovie.setVisibility(View.VISIBLE);
+            pbrMovieList.setVisibility(View.INVISIBLE);
           }
-
-          rvwGridMovie.setVisibility(View.VISIBLE);
-          pbrMovieList.setVisibility(View.INVISIBLE);
-        }
-
-        @Override
-        public void onFailure(Call<List_MovieObjects> call, Throwable t) {
-          Log.e(LOG_TAG, t.toString());
-        }
-      });
-    }
-    else {
+      
+          @Override
+          public void onFailure(Call<List_MovieObjects> call, Throwable t) {
+            Log.e(LOG_TAG, t.toString());
+          }
+        });
+      } else {
       /* THIS IS THE OLD WAY (CONSUME DBHELPER & DB INSTANTLY) */
 //      MovieFavoriteDbHelper dbHelper = new MovieFavoriteDbHelper(getActivity().getApplicationContext());
 //      SQLiteDatabase db = dbHelper.getReadableDatabase();
@@ -121,19 +131,29 @@ public class FragmentGridMovie extends Fragment implements AdapterMovieObject.cl
 //      List<MovieObject> listMovie = dbHelper.getAllMovieObject(db);
 //      db.close();
 //      /* END OF OLD WAY */
-
-      List<MovieObject> listMovie = allocateListMovieObject();
-      if(listMovie != null) {
-        allocateRecyclerView(listMovie, view);
+    
+        listMovie = allocateListMovieObject();
+        if (listMovie != null) {
+          allocateRecyclerView(listMovie, view);
+        }
+    
+        rvwGridMovie.setVisibility(View.VISIBLE);
+        pbrMovieList.setVisibility(View.INVISIBLE);
       }
-
-      rvwGridMovie.setVisibility(View.VISIBLE);
-      pbrMovieList.setVisibility(View.INVISIBLE);
     }
 
     return view;
   }
-
+  
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    if(listMovie != null && listMovie.size() > 0) {
+      ArrayList<MovieObject> arrListMovie = new ArrayList<>(listMovie);
+      outState.putParcelableArrayList(SAVE_TAG, arrListMovie);
+    }
+  }
+  
   private void allocateRecyclerView(List<MovieObject> listMovie, View view) {
     AdapterMovieObject adpMovieObject = new AdapterMovieObject(listMovie);
     adpMovieObject.setOnImageViewClick(FragmentGridMovie.this);
